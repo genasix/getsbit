53b4723048c3c08842c2675f18a65d7a7546dc97
woori <genasix1@gmail.com>
2018-08-27 02:05:32 +0900

    highstock 추가
    + candle 차트 구현
    + upbit api 사용

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChartModule } from 'angular-highcharts';
 import { ChartComponent } from './chart-component/chart/chart.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ChartComponent } from './chart-component/chart/chart.component';
   imports: [
     BrowserModule,
     ChartModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/chart-component/chart/chart.component.ts
+++ frontend/src/app/chart-component/chart/chart.component.ts
@@ -1,63 +1,94 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges,ElementRef, ViewChild} from '@angular/core';
 import {bitChart, bitData} from "../data/bit-data";
 import {Chart} from "angular-highcharts";
-declare var $: any;
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import * as Highcharts from 'highcharts/highstock';
+import {ChartObject} from "highcharts/highstock";
+
+//
+// const groupingUnits = [[
+//   'minute',                         // unit name
+//   [1]                             // allowed multiples
+// ], [
+//   'month',
+//   [1, 2, 3, 4, 6]
+// ]];
+
 
 @Component({
   selector: 'app-chart',
     template: `
-    <div [chart]="chart"></div>
+    <!--<div [chart]="chart"></div>-->
+    <div #container></div>
   `
 })
 
-export class ChartComponent implements OnInit {
-  chartData = new Array;
-  chart = new Chart(bitChart);
+export class ChartComponent {
 
+  chart : ChartObject;
+  name = 'Angular 5 Highstock';
+  @ViewChild("container", {read: ElementRef}) container: ElementRef;
 
-  constructor() {
-    this.addChartData();
-    this.chart.addSerie({
-      // type: 'candlestick',
-      data: this.chartData,
-      marker: {
-        enabled: true,
-        radius: 3,
-        symbol: 'circle'
-      },
-      name: 'bit'
-    });
-    // this.requestTest()
+  constructor(private http: HttpClient) {
+
+    this.http.get('https://crix-api-endpoint.upbit.com/v1/crix/candles/lines?code=CRIX.UPBIT.KRW-BTC').subscribe(json => {
+      this.chart = Highcharts.stockChart(this.container.nativeElement, bitChart);
+      let data = this.paseUpbitData(json);
+
+      this.chart.addSeries(
+        {
+          type: 'candlestick',
+          name: 'AAPL',
+          data: data['candleData'],
+          // dataGrouping: {
+          //   units: groupingUnits
+          // },
+        },
+      );
+
+      this.chart.addSeries(
+        {
+          type: 'column',
+          name: 'Volume',
+          data: data['volumeData'],
+          yAxis: 1,
+          // dataGrouping: {
+          //   units: groupingUnits
+          // }
+        },
+      )}
+    );
   }
 
 
+  paseData(data) {
+    let parsedData = [];
 
-  addChartData() {
-    for(let v of bitData["values"]){
-      this.chartData.push([v['x'], v['y']])
+    for (let v of data) {
+      let date = (v['x'] + 32400) * 1000;
+      parsedData.push([date, v['y']])
     }
+    return parsedData;
   }
 
-  // requestTest() {
-  //   var result = undefined;
-  //   $.ajax({
-  //     type: "GET",
-  //     url: "https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json",
-  //     cache: false,
-  //     async:false,
-  //     success: function (data) {
-  //       result = data;
-  //       console.log(data)
-  //     },
-  //     error: function (request,status,error) {
-  //       // putMessage("REST API[" + url + "] 호출 후 에러 발생.");
-  //       alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
-  //     }
-  //   });
-  //   return result;
-  // }
+  paseUpbitData(data){
+    let candleData = [];
+    let volumeData = [];
+    for(let v of data['candles']){
+      candleData.push([v['timestamp'],v['openingPrice'],v['highPrice'],v['lowPrice'],v['tradePrice']]);
+      volumeData.push([v['timestamp'], v['candleAccTradeVolume']])
+    }
+    let parsedData = {
+      "candleData" : candleData,
+      "volumeData" : volumeData
+    };
+    console.log(data);
 
-  ngOnInit(){
+    return parsedData;
+  }
 
+
+  test(){
+    this.http.get('https://crix-api-endpoint.upbit.com/v1/crix/candles/lines?code=CRIX.UPBIT.KRW-BTC').subscribe(json => this.paseUpbitData(json))
   }
 }
